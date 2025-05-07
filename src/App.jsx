149c401be95f7473d077650f06ee1d12f2c4cac1
oanskyy <oana.anghelachi@gmail.com>
2025-05-07 16:34:10 +0100

    refactor: destructure CORE_CONCEPTS properties in CoreConcepts mapping
    - Simplified the mapping logic by destructuring 'image', 'title', and 'description' directly from CORE_CONCEPTS.
    - Improved code readability and maintainability.

--- src/App.jsx
+++ src/App.jsx
@@ -14,12 +14,12 @@ export function Header() {
 	)
 }
 
-export function CoreConcepts(props) {
+export function CoreConcepts({ image, title, description }) {
 	return (
 		<li>
-			<img src={props.image} alt={props.title} />
-			<h3>{props.title}</h3>
-			<p>{props.description}</p>
+			<img src={image} alt={title} />
+			<h3>{title}</h3>
+			<p>{description}</p>
 		</li>
 	)
 }
